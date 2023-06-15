3f04619e7df517a8db9415485ddd36fdd79f2c96
Hugo <hugogrhe@gmail.com>
2023-06-15 19:44:35 +0200

    feat: Error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,24 @@
-<!-- Todo: Fix an error page! -->    
+<script>
+	import { page } from '$app/stores';
+	$: status = $page.status;
+</script>
+
+<div class="container">
+    <h1>{status}</h1>
+    <p>{$page.error?.message}</p>
+</div>
+
+
+<style lang="scss">
+    .container {
+        height: calc(100vh - 70px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    h1 {
+        font-size: 3.4rem;
+    }
+</style>
