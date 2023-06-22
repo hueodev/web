ec849a519e87c86ec1536449f3704af16feabbfd
Hugooo <hugogrhe@gmail.com>
2023-06-22 12:31:49 +0200

    fix: scss

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import preprocessor from "svelte-preprocess"
 
 export default {
+    preprocess: preprocessor(),
     kit: {
         adapter: adapter({
             // See below for an explanation of these options
