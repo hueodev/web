ee2e085f0459427a1e5c0538df512992ce940557
Hugooo <hugogrhe@gmail.com>
2023-07-08 01:34:17 +0200

    style: error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,9 +3,17 @@
   let status = $page.status;
 </script>
 
+<svelte:head>
+  <title>Error {status}</title>
+</svelte:head>
+
 <div class="container">
   <h1>{status}</h1>
-  <p>{$page.error?.message}</p>
+  {#if status === 404}
+    <p>The page you visited does not exist.<br>Please try again later or <a href="/">go back to the home page</a>.</p>
+  {:else}
+    <p>{$page.error?.message}</p>
+  {/if}
 </div>
 
 <style lang="scss">
@@ -19,5 +27,21 @@
 
   h1 {
     font-size: 3.4rem;
+  }
+
+  p {
+    margin: .6rem;
+    font-size: 1.2rem;
+    text-align: center;
+  }
+
+  a {
+    color: rgb(75, 132, 182);
+    text-decoration: none;
+    font-weight: bold;
+    transition: all 250ms ease-in-out;
+    &:hover {
+      color: rgba(75, 132, 182, 0.7);
+    }
   }
 </style>
